bdeaa3ecd8065a458ebc9eac396626dae8424122
Yuriy Galych <63240166+galichy79@users.noreply.github.com>
2022-09-27 20:12:11 +0300

    Add hash validation

--- app.rb
+++ app.rb
@@ -26,25 +26,24 @@ post '/visit' do
   @datetime = params[:datetime]
   @barber = params[:barber]
   @color = params[:color]
-
-  if @username == ''
-    @error = 'Введите имя'
+  # хеш
+  hh = { :username => 'Введите имя',
+         :phone => 'Введите телефон',
+         :datetime => 'Введите дату и время'}
   
-  end  
-
-  if @phone == ''
-    @error = 'Введите номер телефона'
-   
+  
+  # для каждой пары ключ-значение
+  hh.each do |key, value|
+    # если параметр пуст
+    if params[key] == ''
+      # переменной error присвоить value из хеша hh
+      # (а value из хеша hh это сообщение об ошибке)
+      # т.е. переменной error присвоить сообщение об ошибке.
+      @error = hh[key]
+      # вернуть представление visit
+      return erb :visit
   end
-
-  if @datetime == ''
-    @error = 'Неправильная дата и время'
   end
-
-  if @error != ''
-    return erb :visit
-  end
-
 
   erb "Хорошо,  #{@username}, тел. #{@phone}, время #{@datetime}, парикмахер #{@barber}, цвет #{@color}. Мы вас записали к парикмахеру #{@barber} на #{@datetime}."
 end
